26ee45ef9f3dfd362f7ea86ce82275d8dc1a7685
Armin <armin3600z@gmail.com>
2022-10-18 12:39:27 +0100

    Seperated controls from movement script

--- scripts/movement.js
+++ scripts/movement.js
@@ -147,36 +147,3 @@ function EvaluateMovement(){
         }
     }
 }
-
-// Keypress detection
-document.addEventListener("keydown",  function onEvent(event){
-    // Go right
-    if (event.key == "d" || event.key == "D"){
-        movementKeyStates.d = true;
-   }
-    
-    // Go left
-    else if (event.key == "a" || event.key == "A"){
-        movementKeyStates.a = true;
-    }
-    // Go up
-    else if ((event.key == "w" || event.key == "W" || event.key == " ") && player.canJump == true){
-        movementKeyStates.w = true;
-    }
-});
-
-document.addEventListener("keyup",  function onEvent(event){
-    // Stop right
-    if (event.key == "d" || event.key == "D"){
-        movementKeyStates.d = false;
-    }
-    // Stop left
-    else if (event.key == "a" || event.key == "A"){
-        movementKeyStates.a = false;
-    }
-    // Go up
-    else if (event.key == "w" || event.key == "W" || event.key == " "){
-        movementKeyStates.w = false;
-        player.jumpHeight = 0;
-    }
-});
